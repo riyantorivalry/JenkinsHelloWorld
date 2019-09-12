39253516bb9e24ce3c760aba961028db9b914fc0
riyantorivalry <riyanto.rivalry@gmail.com>
2019-09-12 14:45:19 +0700

    confirming jenkins is running

--- JenkinsHelloWorld.java
+++ JenkinsHelloWorld.java
@@ -1,6 +1,9 @@
 public class JenkinsHelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, Jenkins !");
+		System.out.println("Hi, World !");
+		
+		System.out.println("Yess...working !");
     }
     
 }
